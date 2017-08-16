9889b791bf3b314624d2194b00ba573e5d54ea6e
Juan Marcos Ottonello <jmottonello@gmail.com>
2017-08-16 22:37:37 +0200

    Fix: Trivial implementation circling the circuit by using Frenet coordinates

--- src/main.cpp
+++ src/main.cpp
@@ -209,12 +209,12 @@ int main() {
 
       if (s != "") {
         auto j = json::parse(s);
-        
+
         string event = j[0].get<string>();
-        
+
         if (event == "telemetry") {
           // j[1] is the data JSON object
-          
+
         	// Main car's localization Data
           	double car_x = j[1]["x"];
           	double car_y = j[1]["y"];
@@ -226,7 +226,7 @@ int main() {
           	// Previous path data given to the Planner
           	auto previous_path_x = j[1]["previous_path_x"];
           	auto previous_path_y = j[1]["previous_path_y"];
-          	// Previous path's end s and d values 
+          	// Previous path's end s and d values
           	double end_path_s = j[1]["end_path_s"];
           	double end_path_d = j[1]["end_path_d"];
 
@@ -238,6 +238,14 @@ int main() {
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+            double dist_inc = 0.5;
+            for(int i =0; i< 50; i++) {
+                double next_s = car_s +(i+1) * dist_inc;
+                double next_d = 6;
+                vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x,map_waypoints_y);
+                next_x_vals.push_back(xy[0]);
+                next_y_vals.push_back(xy[1]);
+            }
 
           	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
           	msgJson["next_x"] = next_x_vals;
@@ -247,7 +255,7 @@ int main() {
 
           	//this_thread::sleep_for(chrono::milliseconds(1000));
           	ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-          
+
         }
       } else {
         // Manual driving
